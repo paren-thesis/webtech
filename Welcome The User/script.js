c44f73bb3965017fbba6183493758e82961df6ec
p@ren <ntimprosper308@gmail.com>
2024-11-21 17:05:51 +0000

    Add Welcome Project

---
+++ Welcome The User/script.js
@@ -0,0 +1,6 @@
+function showDialog() {
+    var fname = document.forms["myForm"]["fname"].value;
+    var lname = document.forms["myForm"]["lname"].value;
+
+    alert("Hello there " + fname + " " + lname);
+}
