a387c0c0bfafc625b3b7d2d938375b7adee3a7b4
p@ren <ntimprosper308@gmail.com>
2024-12-12 17:10:26 +0000

    Add ShowDishes project
    Created a prompt to alert user to select a food from a menu

---
+++ ShowDishes/script/script.js
@@ -0,0 +1,23 @@
+let choice = prompt("Choose from the list of dishes using the numbers \n1. Jollof Rice and chicken \n2. Banku with Okro Soup \n3. Kenkey with fish \n4. Sandwich \n5. Gob3");
+
+if (choice >= 1 && choice <= 5) {
+    let food = null;
+    if (choice == 1) {
+        food = "Jollof Rice and chicken"
+    }
+    if (choice == 2) {
+        food = "Banku with Okro Soup"
+    }
+    if (choice == 3) {
+        food = "Kenkey with fish"
+    }
+    if (choice == 4) {
+        food = " Sandwich"
+    }
+    if (choice == 5) {
+        food = "Gob3"
+    }
+    alert('Enjoy your ' + food);
+} else {
+    alert('This dish is not in the menu');
+}
