8c695059695559e01d82fe985737819d3d7f397e
p@ren <83308376+paren-thesis@users.noreply.github.com>
2025-01-03 03:10:42 +0000

    Update script.js

--- ShowDishes/script/script.js
+++ ShowDishes/script/script.js
@@ -1,4 +1,4 @@
-let choice = prompt("Choose from the list of dishes using the numbers \n1. Jollof Rice and chicken \n2. Banku with Okro Soup \n3. Kenkey with fish \n4. Sandwich \n5. Gob3 \n6. Fried Rice");
+let choice = prompt("Choose from the list of dishes using the numbers \n1. Jollof Rice and chicken \n2. Banku with Okro Soup \n3. Kenkey with fish \n4. Sandwich \n5. Gob3 with rice \n6. Fried Rice");
 
 if (choice >= 1 && choice <= 5) {
     let food = null;
@@ -15,7 +15,7 @@ if (choice >= 1 && choice <= 5) {
         food = " Sandwich"
     }
     if (choice == 5) {
-        food = "Gob3"
+        food = "Gob3 with rice"
     }
     if (choice == 6) {
         food = "Fried Rice"
