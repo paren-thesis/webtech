99241ac1de0b819aa33214651a1e2be5e02ce46b
p@ren <ntimprosper308@gmail.com>
2025-01-20 17:05:10 +0000

    Add and Modify file

--- Solutions/js/js.js
+++ Solutions/js/js.js
@@ -1,21 +1,42 @@
 
+        // function CallChef() {
+        //     let hasCookbook = prompt("Do you have a Cookbook? (yes/no)");
+        //     if (hasCookbook.toLowerCase() === "yes") {
+        //         alert("This is good news, it means you may likely have the recipe we are looking for.");
+        //         let hasChiliRecipe = prompt("So then, do you have the Chili Recipe? (yes/no)");
+        //         if (hasChiliRecipe.toLowerCase() === "yes") {
+        //             alert("This means you can go ahead and cook some pepperoni meal.");
+        //         } else {
+        //             let hasInternet = prompt("But do you have internet Access? (yes/no)");
+        //             if (hasInternet.toLowerCase() === "yes") {
+        //                 alert("This means we can locate some beautiful Chili Recipes online.");
+        //             } else {
+        //                 alert("Case closed. We may never cook the chili food.");
+        //             }
+        //         }
+        //     } else {
+        //         alert("Case closed. We may never cook the chili food.");
+        //     }
+        // }
+
+        //This is 
+
         function CallChef() {
-            let hasCookbook = prompt("Do you have a Cookbook? (yes/no)");
-            if (hasCookbook.toLowerCase() === "yes") {
-                alert("This is good news, it means you may likely have the recipe we are looking for.");
-                let hasChiliRecipe = prompt("So then, do you have the Chili Recipe? (yes/no)");
-                if (hasChiliRecipe.toLowerCase() === "yes") {
-                    alert("This means you can go ahead and cook some pepperoni meal.");
+            let hasBook = prompt("Do you have a cookbook? (yes/no)");
+            if (hasBook.toLowerCase() === "yes") {
+                alert("This is good news");
+                let hasRecipe = prompt("Do you have the recipe? (yes/no)");
+                if (hasRecipe.toLowerCase() === "yes") {
+                    alert("Then we can cook");
                 } else {
-                    let hasInternet = prompt("But do you have internet Access? (yes/no)");
+                    let hasInternet = prompt("Do you have internet access? (yes/no)");
                     if (hasInternet.toLowerCase() === "yes") {
-                        alert("This means we can locate some beautiful Chili Recipes online.");
+                        alert("We can get some recipes");
                     } else {
-                        alert("Case closed. We may never cook the chili food.");
+                        alert("Case closed we can never cook the food");
                     }
                 }
             } else {
-                alert("Case closed. We may never cook the chili food.");
+                alert("Case closed")
             }
         }
-    
